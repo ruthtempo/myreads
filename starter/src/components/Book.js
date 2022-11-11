0d9ea97ad2bb4f78ff12d840a1a4b785b299b9ec
Ruth Temporal <ruthtemp@gmail.com>
2022-11-11 12:42:17 +0100

    search call

--- starter/src/components/Book.js
+++ starter/src/components/Book.js
@@ -1,6 +1,6 @@
 import { ShelfChanger } from "./ShelfChanger";
 
-export const Book = () => {
+export const Book = ({ book }) => {
   return (
     <div className="book">
       <div className="book-top">
@@ -9,14 +9,13 @@ export const Book = () => {
           style={{
             width: 128,
             height: 193,
-            backgroundImage:
-              'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")',
+            backgroundImage: 'url( "${book.previewLink}")',
           }}
         ></div>
         <ShelfChanger />
       </div>
-      <div className="book-title">To Kill a Mockingbird</div>
-      <div className="book-authors">Harper Lee</div>
+      <div className="book-title">{book.title}</div>
+      <div className="book-authors">{book.authors}</div>
     </div>
   );
 };
--- starter/src/pages/SearchPage.js
+++ starter/src/pages/SearchPage.js
@@ -1,8 +1,24 @@
-import { useState } from "react";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { search } from "../BooksAPI";
+import { Book } from "../components/Book";
 
-export const SearchPage = () => {
-  const [showSearchPage, setShowSearchpage] = useState(false);
+export const SearchPage = ({ library }) => {
+  const [query, setQuery] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  console.log(searchResults);
+  const handleQuery = (event) => {
+    setQuery(event.target.value);
+  };
+
+  useEffect(() => {
+    search(query, 10)
+      .then((results) =>
+        results.error ? setSearchResults([]) : setSearchResults(results)
+      )
+      .catch((err) => console.log("search error", err));
+  }, [query]);
+
   return (
     <div className="search-books">
       <div className="search-books-bar">
@@ -10,11 +26,24 @@ export const SearchPage = () => {
           Close
         </Link>
         <div className="search-books-input-wrapper">
-          <input type="text" placeholder="Search by title, author, or ISBN" />
+          <input
+            type="text"
+            placeholder="Search by title, author, or ISBN"
+            value={query}
+            onChange={handleQuery}
+          />
         </div>
       </div>
+
       <div className="search-books-results">
-        <ol className="books-grid"></ol>
+        <ol className="books-grid">
+          {searchResults.length > 0 &&
+            searchResults.map((book) => (
+              <li key={book.id}>
+                <Book book={book} />
+              </li>
+            ))}
+        </ol>
       </div>
     </div>
   );
