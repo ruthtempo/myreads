e4d318c6e231bd3b57eee234a4df02565f0a3b4b
Ruth Temporal <ruthtemp@gmail.com>
2022-11-14 11:36:16 +0100

    dynamic option render

--- src/components/ShelfChanger.js
+++ src/components/ShelfChanger.js
@@ -1,27 +1,48 @@
 import PropTypes from "prop-types";
+import { useState } from "react";
 
-export const ShelfChanger = ({ currentShelf, book, upsertBook }) => {
+const options = [
+  {
+    value: "currentlyReading",
+    text: "Currently Reading",
+  },
+  {
+    value: "wantToRead",
+    text: "Want to Read",
+  },
+  {
+    value: "read",
+    text: "Read",
+  },
+  {
+    value: "none",
+    text: "None",
+  },
+];
+
+export const ShelfChanger = ({ book, upsertBook }) => {
   const handleChange = (e) => {
+    setSelectedOption(e.target.value);
     upsertBook(book, e.target.value);
   };
 
+  const [selectedOption, setSelectedOption] = useState(book.shelf);
+
   return (
     <div className="book-shelf-changer">
-      <select value={currentShelf} onChange={handleChange}>
-        <option value="nada" disabled>
+      <select value={selectedOption} onChange={handleChange}>
+        <option value="" disabled>
           Move to...
         </option>
-        <option value="currentlyReading">Currently Reading</option>
-        <option value="wantToRead">Want to Read</option>
-        <option value="read">Read</option>
-        <option value="none">None</option>
+        {options.map((option) => (
+          <option value={option.value}>{option.text}</option>
+        ))}
       </select>
     </div>
   );
 };
 
 ShelfChanger.propTypes = {
-  currentShelf: PropTypes.string.isRequired,
   book: PropTypes.object.isRequired,
   upsertBook: PropTypes.func.isRequired,
 };
